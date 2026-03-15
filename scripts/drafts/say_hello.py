6af64b7c1df261739a1916cd2eb34e24465956ed
Anna Wrobel <anek@Annas-MacBook-Pro.local>
2026-03-16 00:43:00 +0100

    Exercise 1.3 finished

--- scripts/drafts/say_hello.py
+++ scripts/drafts/say_hello.py
@@ -2,14 +2,20 @@
 This script reads a list of people and their courses, and writes a greeting for each person who is taking a specific course to a text file.
 """
 import json
+from types import SimpleNamespace
 
 # Load the list of people from the external JSON file
 # PILLAR: Data (the file content), Environment (the file path 'data/raw/people.json'), Code (open/json.load)
 with open('data/raw/people.json', 'r') as f:
     people = json.load(f)
 
-# Course we are interested in - configuration to be put in config file as a json or yaml file
-course = 'pSciComp'
+# Load the configuration data
+with open('config/config.json', 'r') as f:
+    config_data = json.load(f)
+
+# Parametrise JSON keys and variables using SimpleNamespace
+cfg = SimpleNamespace(**config_data)
+
 # Declaration of the list to store greetings - Code
 greetings = []
 """
@@ -18,11 +24,12 @@ Could be moved as a function into srcs but is so simple
 """
 # initating the loop to check if the person is taking the course and if so, add a greeting to the greetings list - Code
 for person in people:
-    # 'courses' is the key word from the config, need to replace it with the variable
-    # if course in person[cfg.courses]:
-    if course in person['courses']:
-        # greetings.append(f"Hello {config.name}\n")
-        greetings.append(f"Hello {person['name']}\n")
+    # cfg.course is the target value ('pSciComp')
+    # cfg.courses is the key ('courses')
+    if cfg.course in person[cfg.courses]:
+        # cfg.name is the key ('name')
+        greetings.append(f"Hello {person[cfg.name]}\n")
+        
 # Write the greetings to a text file
 # Code instruct to open the file, what file to open is environment, 'w' is configuration 
 with open('data/final/greeting.txt', 'w') as f:
