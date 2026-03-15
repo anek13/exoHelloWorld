04288aa077700c5c98f6d2fec61017214a8a3ee7
Anna Wrobel <anek@Annas-MacBook-Pro.local>
2026-03-16 00:47:20 +0100

    Exercise 1.6

--- scripts/drafts/say_hello.py
+++ scripts/drafts/say_hello.py
@@ -1,17 +1,14 @@
 """
 This script reads a list of people and their courses, and writes a greeting for each person who is taking a specific course to a text file.
 """
-import json
+from exohw.utils import load_json_file
 from types import SimpleNamespace
 
-# Load the list of people from the external JSON file
-# PILLAR: Data (the file content), Environment (the file path 'data/raw/people.json'), Code (open/json.load)
-with open('data/raw/people.json', 'r') as f:
-    people = json.load(f)
+# Load the list of people using the reusable function
+people = load_json_file('../../data/raw/people.json')
 
-# Load the configuration data
-with open('config/config.json', 'r') as f:
-    config_data = json.load(f)
+# Load the configuration data using the reusable function
+config_data = load_json_file('../../config/config.json')
 
 # Parametrise JSON keys and variables using SimpleNamespace
 cfg = SimpleNamespace(**config_data)
@@ -29,7 +26,7 @@ for person in people:
     if cfg.course in person[cfg.courses]:
         # cfg.name is the key ('name')
         greetings.append(f"Hello {person[cfg.name]}\n")
-        
+
 # Write the greetings to a text file
 # Code instruct to open the file, what file to open is environment, 'w' is configuration 
 with open('data/final/greeting.txt', 'w') as f:
